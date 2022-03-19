7ac063f1bde86234940e5edb1a8b4a5bb9ff751c
Антон Кочмак <Toha-99999@mail.ru>
2022-03-19 13:46:22 +0300

    Добавьте файлы проекта.

---
+++ Laba3/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace Laba3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 24);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(135, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Выбрать изображение";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(12, 63);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(700, 700);
+            this.panel1.TabIndex = 1;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(718, 63);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(700, 700);
+            this.panel2.TabIndex = 2;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "2",
+            "4",
+            "8",
+            "16",
+            "32",
+            "64"});
+            this.comboBox1.Location = new System.Drawing.Point(308, 25);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 5;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(0, 763);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(1429, 10);
+            this.label1.TabIndex = 6;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(153, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(154, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Число уровней квантования:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(435, 28);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(113, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Метод обработки по:";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Цвету",
+            "Яркости"});
+            this.comboBox2.Location = new System.Drawing.Point(554, 25);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 2;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1429, 773);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(1445, 812);
+            this.Name = "Form1";
+            this.Text = "Лабораторная работа 1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox2;
+    }
+}
+
---
+++ Laba3/Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenCvSharp;
+using System.IO;
+using System.Diagnostics;
+
+namespace Laba3
+{
+    public partial class Form1 : Form
+    {
+        const string originalImagePath = "Original.png";
+        const string quantImagePath = "QuantImage.png";
+        Image image;
+        Image quantImage;
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.SelectedIndex = 0;
+            comboBox2.SelectedIndex = 0;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Выбор изображения
+            OpenFileDialog openDialog = new OpenFileDialog();
+            openDialog.Filter = "Файлы изображений|*.bmp;*.png;*.jpg";
+            if (openDialog.ShowDialog() != DialogResult.OK)
+                return;
+            image = Image.FromFile(openDialog.FileName);
+            image.Save(originalImagePath);
+            image.Dispose();
+            QuantHist();
+            panel2.Invalidate();
+            panel1.Invalidate();
+        }
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            if (image == null)
+                return;
+            using (image = Image.FromFile(originalImagePath))
+            {
+                int s1 = image.Height * 700 / image.Width;
+                e.Graphics.DrawImage(
+                    image,
+                    panel1.AutoScrollPosition.X,
+                    panel1.AutoScrollPosition.Y,
+                    700, s1);
+            }
+        } // Панели для отрисовки изображений
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+            if (quantImage == null)
+                return;
+            using (quantImage = Image.FromFile(quantImagePath))
+            {
+                int s1 = quantImage.Height * 700 / quantImage.Width;
+                e.Graphics.DrawImage(
+                    quantImage,
+                    panel2.AutoScrollPosition.X,
+                    panel2.AutoScrollPosition.Y,
+                    700, s1);
+            }
+        }
+        private void QuantHist()
+        {
+            if (image == null)
+                return;
+            //Создание квантованного изображения
+            using (Mat quant = new Mat())
+            {
+                Kmeans(Cv2.ImRead(originalImagePath), quant, Convert.ToInt32(comboBox1.SelectedItem), comboBox2.SelectedIndex);
+                Cv2.ImWrite(quantImagePath, quant);
+                quantImage = Image.FromFile(quantImagePath);
+                quantImage.Dispose();
+            }
+        }
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            QuantHist();
+            panel2.Invalidate();
+        }
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            QuantHist();
+            panel2.Invalidate();
+        }
+        public static void Kmeans(Mat input, Mat result, int k, int cb2) // Метод для квантования изображение
+        {
+            if (cb2 == 1)
+            {
+                Cv2.CvtColor(input, input, ColorConversionCodes.BGR2HSV);
+            }
+            using (Mat points = new Mat())
+            {
+                using (Mat labels = new Mat())
+                {
+                    using (Mat centers = new Mat())
+                    {
+                        int width = input.Cols;
+                        int height = input.Rows;
+
+                        points.Create(width * height, 1, MatType.CV_32FC3);
+                        centers.Create(k, 1, points.Type());
+                        result.Create(height, width, input.Type());
+
+                        int i = 0;
+                        for (int y = 0; y < height; y++)
+                        {
+                            for (int x = 0; x < width; x++, i++)
+                            {
+                                Vec3f vec3f = new Vec3f
+                                {
+                                    Item0 = input.At<Vec3b>(y, x).Item0,
+                                    Item1 = input.At<Vec3b>(y, x).Item1,
+                                    Item2 = input.At<Vec3b>(y, x).Item2
+                                };
+
+                                points.Set<Vec3f>(i, vec3f);
+                            }
+                        }
+
+                        Cv2.Kmeans(
+                            points,
+                            k, labels,
+                            new TermCriteria(CriteriaTypes.Eps |
+                            CriteriaTypes.MaxIter, 10, 1.0),
+                            3, KMeansFlags.PpCenters, centers);
+
+                        i = 0;
+                        for (int y = 0; y < height; y++)
+                        {
+                            for (int x = 0; x < width; x++, i++)
+                            {
+                                int idx = labels.Get<int>(i);
+
+                                Vec3b vec3b = new Vec3b();
+                                int tmp = 0;
+                                if (cb2 == 1) //Квантование по яркости
+                                {
+                                    vec3b.Item0 = input.At<Vec3b>(y, x).Item0;
+                                    vec3b.Item1 = input.At<Vec3b>(y, x).Item1;
+                                }
+                                else //Квантование по цвету
+                                {
+                                    tmp = Convert.ToInt32(Math.Round(centers.At<Vec3f>(idx).Item0));
+                                    tmp = tmp > 255 ? 255 : tmp < 0 ? 0 : tmp;
+                                    vec3b.Item0 = Convert.ToByte(tmp);
+
+                                    tmp = Convert.ToInt32(Math.Round(centers.At<Vec3f>(idx).Item1));
+                                    tmp = tmp > 255 ? 255 : tmp < 0 ? 0 : tmp;
+                                    vec3b.Item1 = Convert.ToByte(tmp);
+                                }
+
+                                tmp = Convert.ToInt32(Math.Round(centers.At<Vec3f>(idx).Item2));
+                                tmp = tmp > 255 ? 255 : tmp < 0 ? 0 : tmp;
+                                vec3b.Item2 = Convert.ToByte(tmp);
+
+                                result.Set<Vec3b>(y, x, vec3b);
+
+                            }
+                        }
+
+                    }
+                }
+            }
+            if (cb2 == 1)
+            {
+                Cv2.CvtColor(result, result, ColorConversionCodes.HSV2BGR);
+            }
+        }
+    }
+}
